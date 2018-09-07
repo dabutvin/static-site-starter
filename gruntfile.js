75493661f6bb2834a4af07f0da4da3aeee8559d4
dabutvin <butvinik@outlook.com>
2018-09-06 20:42:01 -0700

    add site skeleton

---
+++ gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-markdown')
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-devserver')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+
+  grunt.initConfig({
+    markdown: {
+      all: {
+        files: [
+          {
+            cwd: 'src',
+            expand: true,
+            src: '**/*.md',
+            dest: 'dist/.',
+            ext: '.html'
+          },
+          {
+            cwd: 'src',
+            expand: true,
+            src: '**/*.html',
+            dest: 'dist/.',
+            ext: '.html'
+          }
+        ],
+        options: {
+          template: 'src/layout.jst'
+        }
+      }
+    },
+    less: {
+      all: {
+        files: {
+          'dist/css/site.css': 'src/css/site.less'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'dist/css/site.min.css': 'dist/css/site.css'
+        }
+      }
+    },
+    copy: {
+      all: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['favicon.ico', 'images/**'],
+            dest: 'dist'
+          }
+        ]
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/**/*.*'],
+        tasks: ['gen'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    devserver: {
+      dist: {
+        options: {
+          base: 'dist'
+        }
+      }
+    }
+  })
+
+  grunt.registerTask('gen', ['markdown:all', 'less:all', 'cssmin', 'copy'])
+}
